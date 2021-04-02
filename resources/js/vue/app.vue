74575632c2add2d7f5f1e00fb529e8303c99fad1
AmineDevF <amine.devinfo@gmail.com>
2021-04-02 15:48:23 +0100

    first commit (CRUD)

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="todoListContainer">
+      <div class="heading">
+        <h2 id="title">Task manager</h2>
+        <add-Item-Form  v-on:reloadlist = "getList()" />
+  
+      </div>
+          <list-View :items="items" 
+          v-on:reloadlist = "getList()"
+          />
+       
+    </div>
+</template>
+<script>
+
+import addItemForm from './addItemForm'
+
+import listView from './listView'
+export default {
+
+components : {addItemForm ,listView } ,
+data: function() {
+    return {
+        items : []
+    }
+},
+methods : { 
+    getList () {
+    axios.get('api/items')
+    .then(response => {
+        this.items = response.data
+    })
+    .catch( error => {
+        console.log(error) ;
+    })
+
+    }
+},
+created() {
+    this.getList();
+}
+}
+</script>
+
+<style scoped >
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+.heading {
+ padding: 10px;
+ background: #e7dfdf;
+
+} 
+#title {
+    text-align: center;
+     
+}
+
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div v-for="(item , index) in items" :key="index">
+        <list-item 
+        :item="item" 
+        class="item"
+        v-on:itemchanged="$emit('reloadlist')"
+        />
+    </div>
+    </div>
+</template>
+
+<script>
+import listItem from "./listItem"
+import ListItem from './listItem.vue'
+
+export default {
+    props : ['items'],
+ components : {listItem,ListItem }
+     
+  
+ 
+}
+</script>
+
+<style scoped>
+.item {
+    background : rgb(124, 118, 118);
+    padding: 5px;
+    margin-top: 5px;
+}
+</style>
